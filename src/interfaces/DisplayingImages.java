5269ca24e70ecba86720be30cd8b1e372f47afad
Cosmin Crisan <cosmin.v.crisan@gmail.com>
2020-12-23 11:20:45 +0200

    initial commit
    added assets folder, interfaces package, bussinessLogic package and ui package
    
    Co-Authored-By: Nicoara Dan-Alexandru <26961682+danalexandrunicoara@users.noreply.github.com>

---
+++ src/interfaces/DisplayingImages.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package interfaces;
+
+/**
+ * @author Cosmin
+ * 
+ *         loads images
+ */
+public interface DisplayingImages {
+	/**
+	 * loading images from the specified path
+	 * 
+	 * @param path - path to images
+	 */
+	public void loadImages(String path);
+}
