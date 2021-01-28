f71114f9bd0330cf110dbd69e57bdc32c2ce4e48
Dan Nicoara <dan.nicoara@3pillarglobal.com>
2021-01-28 15:45:56 +0200

    Am incercat o refactorizare a codului.
    Aceasta a avut ca scop mutarea codului in businessLogic si interfaces.

---
+++ src/interfaces/ManagesImages.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package interfaces;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * @author Cosmin
+ * 
+ *         loads images
+ */
+public interface ManagesImages {
+	/**
+	 * loading images from the specified path
+	 * 
+	 * @param path       - path to images
+	 * @param imageIndex - the index of the image.
+	 */
+	public BufferedImage loadImages(String path, int imageIndex);
+
+	/**
+	 * Returns the position of the positive image.
+	 * 
+	 * @param imageList
+	 * @return
+	 */
+	public int getPositiveImagePosition(int[] imageList);
+
+	/**
+	 * Set image indexes.
+	 * 
+	 * @param imageList
+	 */
+	public void setIndexImages(int[] imageList);
+}
--- src/ui/StartUp.java
+++ src/ui/StartUp.java
@@ -1,177 +1,164 @@
 package ui;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import bussinessLogic.ImageManager;
+import interfaces.ManagesImages;
 
 public class StartUp extends JPanel {
 
-    // Random object to shuffle tiles
-    private static final Random RANDOM = new Random();
-    // the current number of pictures in the resource folder
-    private static final int numberOfFiles = 14;
-    // Number of images in a row, hence the number of columns
-    private final int numberOfColumns;
-    // Number of rows
-    private final int numberOfRows;
-    // Number of images
-    private final int numberOfImages;
-    // Grid UI Dimension
-    private final int gridDimension;
-    // Array of integers for storing the position of images
-    private final int[] images;
-    // Size of individual image on UI
-    private final int imageSize;
-    // Margin for the grid on the frame
-    private final int gridMargin;
-    // Grid UI Size
-    private final int gridSize;
-    // Position of the positive image
-    private int positiveImagePosition;
+	// Random object to shuffle tiles
+	private static final Random RANDOM = new Random();
 
-    private StartUp(int numberOfColumns, int gridDimension, int gridMargin) {
-        this.numberOfColumns = numberOfColumns;
-        this.gridDimension = gridDimension;
-        this.gridMargin = gridMargin;
-        numberOfRows = numberOfColumns / 4 * 3; // numberOfColumns/4*3 to keep a 4:3 aspect ratio
-        numberOfImages = numberOfColumns * numberOfRows;
-        images = new int[numberOfImages];
+	// Number of images in a row, hence the number of columns
+	private int numberOfColumns;
+	// Number of rows
+	private int numberOfRows;
+	// Number of images
+	private int numberOfImages;
+	// Array of integers for storing the position of images
+	private int[] images;
+	// Size of individual image on UI
+	private int imageSize;
+	// Margin for the grid on the frame
+	private int gridMargin;
+	// Grid UI Size
+	private int gridSize;
+	// Position of the positive image
+	private int positiveImagePosition;
 
-        // calculate grid size and image size
-        gridSize = (gridDimension - 2 * gridMargin);
-        imageSize = gridSize / numberOfColumns;
+	/**
+	 * @see DisplayingImages.
+	 */
+	private ManagesImages imageManager;
 
-        // set the window size to accommodate the 4x3 layout
-        setPreferredSize(new Dimension(gridDimension, imageSize * numberOfRows + 2 * gridMargin));
-        setBackground(Color.WHITE);
+	private StartUp(int numberOfColumns, int gridDimension, int gridMargin) {
+		initialize(numberOfColumns, gridDimension, gridMargin);
 
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                // used for checking if the positive image is clicked
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// get position of the click
+				int ex = e.getX() - gridMargin;
+				int ey = e.getY() - gridMargin;
 
-                // get position of the click
-                int ex = e.getX() - gridMargin;
-                int ey = e.getY() - gridMargin;
+				// click in the grid ?
+				if (ex < 0 || ex > gridSize || ey < 0 || ey > gridSize)
+					return;
 
-                // click in the grid ?
-                if (ex < 0 || ex > gridSize || ey < 0 || ey > gridSize)
-                    return;
+				// get position in the grid
+				int c1 = ex / imageSize;
+				int r1 = ey / imageSize;
 
-                // get position in the grid
-                int c1 = ex / imageSize;
-                int r1 = ey / imageSize;
+				// we convert in the 1D coord
+				int clickPosition = r1 * numberOfColumns + c1;
 
-                // we convert in the 1D coord
-                int clickPosition = r1 * numberOfColumns + c1;
+				// restart the program if the user clicks on the positive image
+				if (clickPosition == positiveImagePosition) {
+					restart();
+					repaint();
+				}
 
-                // restart the program if the user clicks on the positive image
-                if (clickPosition == positiveImagePosition) {
-                    restart();
-                    repaint();
-                }
+			}
+		});
 
-            }
-        });
+		startProgram();
+	}
 
-        startProgram();
-    }
+	/**
+	 * @param numberOfColumns
+	 * @param gridDimension
+	 * @param gridMargin
+	 */
+	private void initialize(int numberOfColumns, int gridDimension, int gridMargin) {
+		this.imageManager = new ImageManager();
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame();
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setTitle("Cognitive Bias Modifier");
-            frame.setResizable(false);
-            frame.add(new StartUp(4, 750, 30), BorderLayout.CENTER);
-            frame.pack();
-            // center on the screen
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
-        });
-    }
+		this.numberOfColumns = numberOfColumns;
+		this.gridMargin = gridMargin;
+		this.numberOfRows = numberOfColumns / 4 * 3; // numberOfColumns/4*3 to keep a 4:3 aspect ratio
+		this.numberOfImages = numberOfColumns * numberOfRows;
+		this.images = new int[numberOfImages];
 
-    // restarts the program
-    private void restart() {
-        startProgram();
-    }
+		// calculate grid size and image size
+		this.gridSize = (gridDimension - 2 * gridMargin);
+		this.imageSize = gridSize / numberOfColumns;
 
-    // starts the program
-    private void startProgram() {
+		// set the window size to accommodate the 4x3 layout
+		setPreferredSize(new Dimension(gridDimension, imageSize * numberOfRows + 2 * gridMargin));
+		setBackground(Color.WHITE);
+	}
 
-        reset(); // reset the array of images
-        shuffle();
-        getPositiveImagePosition();
-    }
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setTitle("Cognitive Bias Modifier");
+			frame.setResizable(false);
+			frame.add(new StartUp(4, 750, 30), BorderLayout.CENTER);
+			frame.pack();
+			// center on the screen
+			frame.setLocationRelativeTo(null);
+			frame.setVisible(true);
+		});
+	}
 
-    private void reset() {
-        for (int i = 0; i < this.images.length; i++) {
-            this.images[i] = (i + 1) % this.images.length; // ads 1, 2, 3...11, 0 to tiles array
-        }
-    }
+	// restarts the program
+	private void restart() {
+		startProgram();
+	}
 
-    private void shuffle() {
-        int n = numberOfImages; // it's 12 now (4x3)
+	// starts the program
+	private void startProgram() {
+		// reset the array of images
+		this.imageManager.setIndexImages(this.images);
+		shuffle();
+		positiveImagePosition = this.imageManager.getPositiveImagePosition(this.images);
+	}
 
-        while (n > 1) {
-            int r = RANDOM.nextInt(n--);
-            int temporary = images[r];
-            images[r] = images[n];
-            images[n] = temporary;
-        }
-    }
+	private void shuffle() {
+		int n = numberOfImages; // it's 12 now (4x3)
 
-    private void getPositiveImagePosition() {
-        for (int i = 0; i < images.length; i++) {
-            if (images[i] == 0) {
-                positiveImagePosition = i;
-                break;
-            }
-        }
-    }
+		while (n > 1) {
+			int r = RANDOM.nextInt(n--);
+			int temporary = images[r];
+			images[r] = images[n];
+			images[n] = temporary;
+		}
+	}
 
-    private void buildImages(Graphics2D g2d) {
+	private void buildImages(Graphics2D g2d) {
 
-        for (int i = 0; i < images.length; i++) {
-            // we convert 1D coords to 2D coords given the size of the 2D Array
-            int r = i / numberOfColumns;
-            int c = i % numberOfColumns;
-            // we convert in coords on the UI
-            int x = gridMargin + c * imageSize;
-            int y = gridMargin + r * imageSize;
+		for (int i = 0; i < images.length; i++) {
+			// we convert 1D coords to 2D coords given the size of the 2D Array
+			int r = i / numberOfColumns;
+			int c = i % numberOfColumns;
+			// we convert in coords on the UI
+			int x = gridMargin + c * imageSize;
+			int y = gridMargin + r * imageSize;
 
-            BufferedImage img;
-            int n = images[i];
-            String path;
+			BufferedImage image = this.imageManager.loadImages("assets/Faces/", images[i]);
+			g2d.drawImage(image, x, y, imageSize, imageSize, null);
+		}
+	}
 
-            if (n == 0) {
-
-                int random = RANDOM.nextInt(numberOfFiles);
-                path = "assets/Faces/Positive/" + random + ".jpg";
-
-            } else
-                path = "assets/Faces/Negative/" + n + ".jpg";
-
-            try {
-                img = ImageIO.read(getClass().getClassLoader().getResource(path));
-                g2d.drawImage(img, x, y, imageSize, imageSize, null);
-            } catch (IOException e) {
-                System.out.println("Error loading image");
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Override
-    protected void paintComponent(Graphics g2d) {
-        super.paintComponent(g2d);
-        Graphics2D g = (Graphics2D) g2d;
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        buildImages(g);
-    }
+	@Override
+	protected void paintComponent(Graphics g2d) {
+		super.paintComponent(g2d);
+		Graphics2D g = (Graphics2D) g2d;
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		buildImages(g);
+	}
 }
